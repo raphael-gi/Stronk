6cd00db6209268d0720ed9bcf75fa0645e3c7288
raphael-gi <raphael.gisler@stud.edubs.ch>
2022-12-01 06:50:26 +0100

    Created separate Controller for Exercise and Workout

---
+++ src/Controllers/ExerciseController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stronk.Repositories;
+using Stronk.Models;
+
+namespace Stronk.Controllers;
+
+[Authorize]
+public class ExerciseController : Controller
+{
+    private readonly ExerciseRepository _exerciseRepository;
+    
+    public ExerciseController(ExerciseRepository exerciseRepository)
+    {
+        _exerciseRepository = exerciseRepository;
+    }
+
+    public ActionResult Index()
+    {
+        ViewBag.Muscles = _exerciseRepository.GetMuscles();
+        ViewBag.Exercises = _exerciseRepository.GetExercises();
+        return View();
+    }
+
+    public ActionResult Create()
+    {
+        ViewBag.Muscles = _exerciseRepository.GetMuscles();
+        return View();
+    }
+
+    [HttpPost]
+    public RedirectResult Create(Exercise exercise, string[] muscle)
+    {
+        muscle = muscle.Where(m => !m.Contains("false")).ToArray();
+        //if (!ModelState.IsValid)
+        //{
+        //    Console.WriteLine(ModelState.ValidationState);
+        //    Console.WriteLine("Model State is not valid");
+        //    return Redirect("/Exercise/Create");
+        //}
+        if (!_exerciseRepository.Add(exercise, muscle))
+        {
+            Console.WriteLine("Exercise couldn't be added");
+            return Redirect("/Exercise/Create");
+        }
+
+        return Redirect("/Exercise");
+    }
+
+    public ActionResult Edit(int id)
+    {
+        Exercise exercise = _exerciseRepository.GetExercise(id);
+        Console.WriteLine(exercise.Name);
+        ViewBag.Exercise = exercise;
+        ViewBag.Muscles = _exerciseRepository.GetMuscles();
+        return View();
+    }
+}
---
+++ src/Controllers/WorkoutController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stronk.Data;
+using Stronk.Models;
+
+namespace Stronk.Controllers;
+
+[Authorize]
+public class WorkoutController : Controller
+{
+    private readonly WorkoutRepository _workoutRepository;
+
+    public WorkoutController(WorkoutRepository workoutRepository)
+    {
+        _workoutRepository = workoutRepository;
+    }
+    public IActionResult Index()
+    {
+        ViewBag.Workouts = _workoutRepository.Workouts();
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Exercises = _workoutRepository.Exercises();
+        return View();
+    }
+
+    [HttpPost]
+    public RedirectResult Create(Workout workout)
+    {
+        return Redirect("/Workout");
+    }
+}
