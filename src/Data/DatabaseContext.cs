345b8ad6804fad4b548914297a5917ec0a39150f
raphael-gi <raphael.gisler@stud.edubs.ch>
2022-12-01 07:05:36 +0100

    Implemented Entity Framework

---
+++ src/Data/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stronk.Models;
+
+namespace Stronk.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Exercise> Exercises { get; set; }
+    public DbSet<Muscle> Muscles { get; set; }
+    public DbSet<ExerciseMuscle> ExercisesMuscles { get; set; }
+    public DbSet<Workout> Workouts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Exercise_Muscle>()
+        //    .HasKey(em => new { em.ExerciseId, em.MusclesId });
+        //
+        //modelBuilder.Entity<Exercise_Muscle>()
+        //    .HasOne(e => e.Exercise)
+        //    .WithMany(em => em.ExerciseMuscles)
+        //    .HasForeignKey(e => e.ExerciseId);
+        //modelBuilder.Entity<Exercise_Muscle>()
+        //    .HasOne(m => m.Muscle)
+        //    .WithMany(em => em.ExerciseMuscles)
+        //    .HasForeignKey(m => m.MusclesId);
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,27 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Stronk.Data;
+using Stronk.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(c =>
+    {
+        c.LoginPath = "/Login";
+        c.AccessDeniedPath = "/Login/Denied";
+    });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<ExerciseRepository>();
+builder.Services.AddScoped<WorkoutRepository>();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -21,12 +37,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
