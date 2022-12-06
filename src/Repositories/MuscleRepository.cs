3ab2e1b04591d937135633343fc989aba2a6a29a
raphael-gi <raphael.gisler@stud.edubs.ch>
2022-12-07 00:06:02 +0100

    Created new Repositories

---
+++ src/Repositories/MuscleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stronk.Data;
+using Stronk.Models;
+
+namespace Stronk.Repositories;
+
+public class MuscleRepository
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public MuscleRepository(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+    
+    public async Task<List<Muscle>> GetMuscles()
+    {
+        return await _databaseContext.Muscles.ToListAsync();
+    }
+    public async Task<List<int>> GetSelectedMuscles(int id)
+    {
+        return await _databaseContext.ExercisesMuscles
+            .Where(em => em.ExerciseId == id)
+            .Select(em => em.MuscleId)
+            .ToListAsync();
+    }
+}
---
+++ src/Repositories/PostRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stronk.Data;
+using Stronk.Models;
+
+namespace Stronk.Repositories;
+
+public class PostRepository
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public PostRepository(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    public async Task<List<Post>> GetPosts()
+    {
+        return await _databaseContext.Posts
+            .Include(p => p.User)
+            .Include(p => p.PostWorkout)
+            .ThenInclude(pw => pw.Workout)
+            .ThenInclude(w => w.WorkoutExercises)
+            .ThenInclude(we => we.Exercise)
+            .ToListAsync();
+    }
+    public async Task<List<Workout>> GetWorkouts()
+    {
+        return await _databaseContext.Workouts.ToListAsync();
+    }
+    public async Task<bool> CreatePost(Post post, int[] workouts)
+    {
+        post.Date = DateTime.Now;
+        List<PostWorkout> postWorkouts = new List<PostWorkout>();
+        foreach (int workout in workouts)
+        {
+            postWorkouts.Add(new PostWorkout
+            {
+                WorkoutId = workout
+            });
+        }
+        post.PostWorkout = postWorkouts;
+        await _databaseContext.Posts.AddAsync(post);
+        return await _databaseContext.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> DeletePost(int id)
+    {
+        return true;
+    }
+}
