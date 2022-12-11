12a2ddf6f849bba0100a60307424588a9ab31773
raphael-gi <raphael.gisler@stud.edubs.ch>
2022-12-11 11:53:32 +0100

    Description can be null

--- src/Models/Exercise.cs
+++ src/Models/Exercise.cs
@@ -8,9 +8,8 @@ public class Exercise
 {
     [Key]
     public int Id { get; set; }
-    [Required] [MaxLength(50)]
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public int UserId { get; set; }
     public User User { get; set; }
     public ICollection<ExerciseMuscle> ExerciseMuscles { get; set; }
